abf01254e3d531054b847a64915f3013d820b58c
efibs <etienne.muser@hotmail.com>
2025-04-27 15:05:06 +0200

    Rewrite home fragment to use view model

--- GeoAppAndroid/app/src/main/java/de/fibs/geoappandroid/ui/home/HomeViewModel.kt
+++ GeoAppAndroid/app/src/main/java/de/fibs/geoappandroid/ui/home/HomeViewModel.kt
@@ -1,13 +1,24 @@
 package de.fibs.geoappandroid.ui.home
 
-import androidx.lifecycle.LiveData
+import androidx.databinding.Bindable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
+    val text = MutableLiveData("This is home Fragment")
+
+    private val _collecting = MutableLiveData(false)
+
+    @get:Bindable
+    var collecting: Boolean
+        get() = _collecting.value ?: false
+        set(value) {
+            _collecting.value = value
+            if (value) {
+                text.value = "Enabled"
+            } else {
+                text.value = "Disabled"
+            }
+        }
 }
